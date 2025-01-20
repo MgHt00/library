aa67af106e6698ad2d65099f1df60741e64a807a
Win Htoo Shwe <143469867+MgHt00@users.noreply.github.com>
2025-01-20 10:59:30 +0630

    Update generals.js

--- iig/interactive-quiz/utils/generals.js
+++ iig/interactive-quiz/utils/generals.js
@@ -1,14 +1,28 @@
 export class Generals {
-  constructor() {
-    
-  }
 
   // To generate a random number
   random(min, max) {
     console.groupCollapsed("random()");
-    const randonNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.info("min:", min, "max:", max,"randonNumber:", randonNumber)
+    if (min > max) {
+      console.error("Invalid range: min should not be greater than max.");
+      return null;
+    }
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.info("min:", min, "max:", max,"randonNumber:", randomNumber)
     console.groupEnd();
-    return randonNumber
+    return randomNumber;
+  }
+
+  // Construct an absolute URL from a relative path
+  constructAbsoluteURL(path) {
+    console.groupCollapsed("constructAbsoluteURL()");
+    if (!path.startsWith('/')) { // ensuring the path starts with a / to avoid any relative path issues.
+      path = '/' + path;
+    }
+    const currentURL = new URL(window.location.href);
+    const absoluteURL = new URL(path, currentURL.origin);
+    console.info("Path:", path, "Absolute URL:", absoluteURL.href);
+    console.groupEnd();
+    return absoluteURL.href;
   }
 }
